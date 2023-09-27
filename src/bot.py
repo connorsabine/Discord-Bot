521a8283095c6fc7d2a58ad8b9d8cfa8182d42e0
connorsabine <connorsabine@icloud.com>
2023-09-26 22:11:55 -0400

    added pinecone

--- src/bot.py
+++ src/bot.py
@@ -6,39 +6,40 @@ import os
 
 # EXTENSIONS
 bot = lightbulb.BotApp(token=os.getenv("BOT_TOKEN"), intents=hikari.Intents.ALL_UNPRIVILEGED | hikari.Intents.MESSAGE_CONTENT | hikari.Intents.GUILD_MEMBERS)
+bot.load_extensions("ext.ai.chatbot-embeds")
 
-bot.load_extensions("db.appenduser")
-bot.load_extensions("db.appendguild")
+# bot.load_extensions("db.appenduser")
+# bot.load_extensions("db.appendguild")
 
-bot.load_extensions("ext.ai.chatbot-new")
-bot.load_extensions("ext.ai.imagine")
+# bot.load_extensions("ext.ai.imagine")
+# bot.load_extensions("ext.ai.chatbot-new")
 
-bot.load_extensions("ext.help")
-# bot.load_extensions("ext.music")
-bot.load_extensions("ext.color")
-# bot.load_extensions("ext.sentiment")
-bot.load_extensions("ext.giphy")
+# bot.load_extensions("ext.help")
+# # bot.load_extensions("ext.music")
+# bot.load_extensions("ext.color")
+# # bot.load_extensions("ext.sentiment")
+# bot.load_extensions("ext.giphy")
 
-# bot.load_extensions("ext.members.join")
-# bot.load_extensions("ext.members.leave")
+# # bot.load_extensions("ext.members.join")
+# # bot.load_extensions("ext.members.leave")
 
-# bot.load_extensions("ext.admin.logging")
-# bot.load_extensions("ext.admin.moderation")
-# bot.load_extensions("ext.admin.configure")
+# # bot.load_extensions("ext.admin.logging")
+# # bot.load_extensions("ext.admin.moderation")
+# # bot.load_extensions("ext.admin.configure")
 
-# bot.load_extensions("ext.quotes.quotes")
-# bot.load_extensions("ext.quotes.fortunes")
-bot.load_extensions("ext.quotes.zenquotes")
+# # bot.load_extensions("ext.quotes.quotes")
+# # bot.load_extensions("ext.quotes.fortunes")
+# bot.load_extensions("ext.quotes.zenquotes")
 
-bot.load_extensions("ext.games.rolldie")
-bot.load_extensions("ext.games.coinflip")
-# bot.load_extensions("ext.games.count")
-bot.load_extensions("ext.games.tictactoevs")
-bot.load_extensions("ext.games.tictactoeai")
-bot.load_extensions("ext.games.rockpaperscissors")
-bot.load_extensions("ext.games.anagrams")
-# bot.load_extensions("ext.games.rpg")
-bot.load_extensions("ext.games.wordle")
+# bot.load_extensions("ext.games.rolldie")
+# bot.load_extensions("ext.games.coinflip")
+# # bot.load_extensions("ext.games.count")
+# bot.load_extensions("ext.games.tictactoevs")
+# bot.load_extensions("ext.games.tictactoeai")
+# bot.load_extensions("ext.games.rockpaperscissors")
+# bot.load_extensions("ext.games.anagrams")
+# # bot.load_extensions("ext.games.rpg")
+# bot.load_extensions("ext.games.wordle")
 
 # Ping Message
 @bot.command
---
+++ src/ext/ai/chatbot-embeds.py
@@ -0,0 +1,123 @@
+from constants import BOT_UID, OPENAI_TOKEN_LIMIT
+import openai
+import hikari
+import lightbulb
+import os
+import pinecone
+from tqdm.auto import tqdm
+
+# PARAMETERS
+EMBED_MODEL = "text-embedding-ada-002"
+COMPLETION_MODEL = "gpt-3.5-turbo-16k"
+
+# INIT
+plugin = lightbulb.Plugin("chatbot")
+
+# OPENAI INIT
+openai.organization = "org-gdi3giKqaf4bCDzRdeTOwEpy"
+openai.api_key = "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"
+
+# PINECONE INIT
+pine = dict()
+pine["api_key"] = "09da0409-864b-4760-a8a7-bd6f8458507f"
+pine["env"] = "gcp-starter"
+pinecone.init(api_key=pine["api_key"], environment=pine["env"])
+
+# REQUIRED FUNCTIONS
+def load(bot):
+    bot.add_plugin(plugin)
+
+def unload(bot):
+    bot.remove_plugin(plugin)
+
+# HELPER FUNCTIONS
+def get_embed(text):
+    return openai.Embedding.create(input=text, engine=EMBED_MODEL)["data"][0]["embedding"]
+
+def get_response(query: str, context: [dict]) -> str:
+    context.append(get_message("user", query))
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo-16k", messages=context, max_tokens=OPENAI_TOKEN_LIMIT)
+    text = response.choices[0].message.content.strip()
+    return text
+
+def get_message(role: str, content: str) -> dict:
+    return {"role": role, "content": content}
+
+def get_messages(role: str, contents: [str]) -> [dict]:
+    context = []
+    for content in contents:
+        context.append(get_message(role, content))
+    return context
+
+def get_index(index: str) -> str:
+    index = str(index)
+    if index not in pinecone.list_indexes():
+        # create sample embedding
+        embed = get_embed(["Sample document text goes here","there will be several phrases in each batch"])
+
+        # create index
+        pinecone.create_index(
+            index,
+            dimension=len(embed),
+            metric='cosine',
+            metadata_config={'indexed': ['source', 'id']}
+        )
+
+        # log creation of index
+        print(f"index {index} created")
+
+    return index
+    
+def search(index, line, n=3):
+    embed = get_embed(line)
+    results = pinecone.index_query(index, embed, top_k=n)
+    return results
+
+def upsert_vector(index: str, line: str):
+    index = pinecone.Index(get_index(index))
+    embed = get_embed(line)
+    meta = {'text': line}
+    index.upsert(vectors=[{
+        'id':'vec1', 
+        'values':embed, 
+        'metadata':meta}])
+    
+def batch_upsert_vector(index: str, data: [str], batch_size=32):
+    index = pinecone.Index(index)
+    for i in tqdm(range(0, len(data), batch_size)):
+        i_end = min(i+batch_size, len(data))
+        lines_batch = data[i: i+batch_size]
+        ids_batch = [str(n) for n in range(i, i_end)]
+        res = get_embed(lines_batch)
+        embeds = [record['embedding'] for record in res['data']]
+        meta = [{'text': line} for line in lines_batch]
+        to_upsert = zip(ids_batch, embeds, meta)
+        index.upsert(vectors=list(to_upsert))
+
+
+@plugin.listener(hikari.MessageCreateEvent)
+async def message_event(event):
+
+    # add vector to index
+    if event.content != None:
+        upsert_vector(event.guild_id, event.content)
+    else:
+        return
+
+    # respond to message if applicable
+    if f"<@{BOT_UID}> " in event.content:
+        query = event.content.replace(f"<@{BOT_UID}> ", "")
+
+        index = get_index(event.guild_id)
+        context = get_messages("assistant", search(index, query, n=3))
+
+        print("context retrieved")
+        print(context)
+
+        await plugin.app.rest.create_message(event.channel_id, get_response(query, context))
+
+
+
+
+        
+
